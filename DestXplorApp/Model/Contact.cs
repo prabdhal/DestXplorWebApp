93e1a305a59e5ce242e245e19251e4805ae71077
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2021-04-11 19:05:06 -0400

    Create Contact.cs model

---
+++ DestXplorApp/Model/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DestXplorApp
+{
+  public class Contact
+  {
+    public int Id { get; set; }
+
+    [Required]
+    [Column(TypeName = "varchar(50)")]
+    public string Name { get; set; }
+
+    [Required]
+    [Column(TypeName = "varchar(50)")]
+    public string Subject { get; set; }
+
+    [Required]
+    public string Message { get; set; }
+  }
+}
